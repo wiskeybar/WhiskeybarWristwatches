7fc5a5df9a8ab1041bef88cca75c1507ceb5d392
Bartek Wisniewski <wis.keybar87@gmail.com>
2019-01-24 20:15:32 +0100

    added more js for navigation and additional styles for typography and navigation

--- scripts.js
+++ scripts.js
@@ -1,33 +1,69 @@
 const menuOpener = document.querySelector('.top_minified__opener')
 const expandedMenu = document.querySelector('.top_expanded');
 const mainNav = document.querySelector('.top_nav')
-menuOpener.addEventListener('click', (e) => {
-    const minifiedMenu = document.querySelector('.top_minified');
-    if (!(minifiedMenu.classList.contains('top_maximized'))) {
-        minifiedMenu.classList.add('top_maximized')
-        menuOpener.innerHTML = "<i class='fas fa-times'>"
+const minifiedMenu = document.querySelector('.top_minified');
+const menuElements = Array.from(document.querySelectorAll('.top_minified__item'))
+const searchButtons = Array.from(document.querySelectorAll('.searchItems'))
+const searchFields = Array.from(document.querySelectorAll('.searchField'))
 
+const openerClose = () => {
+    minifiedMenu.classList.remove('top_maximized');
+    menuOpener.innerHTML = "<i class='fas fa-bars'>";
+    menuOpener.style.color = "black";
+
+}
+const searchInputClose = (i) => {
+    searchFields[i].classList.remove('searchFieldActive')
+}
+
+menuOpener.addEventListener('click', () => {
+    if (!(minifiedMenu.classList.contains('top_maximized'))) {
+        minifiedMenu.classList.add('top_maximized');
+        menuOpener.innerHTML = "<i class='fas fa-times'>";
+        menuOpener.style.color = "white";
     }
     else {
-        minifiedMenu.classList.remove('top_maximized')
-        menuOpener.innerHTML = "<i class='fas fa-bars'>"
-
+        openerClose()
     }
 }
 )
 
 window.addEventListener('scroll', () => {
-    console.log(expandedMenu.offsetHeight, expandedMenu.offsetWidth);
-
-
     if (pageYOffset >= 150) {
-        expandedMenu.classList.add('top_fixed')
-        mainNav.style.marginTop = `${expandedMenu.offsetHeight}px`
-
+        expandedMenu.classList.add('top_fixed');
+        mainNav.style.marginTop = `${expandedMenu.offsetHeight}px`;
     }
     else {
-        expandedMenu.classList.remove('top_fixed')
-        mainNav.style.marginTop = 0
+        expandedMenu.classList.remove('top_fixed');
+        mainNav.style.marginTop = 0;
+    }
+})
+
+window.addEventListener('click', (e) => {
+    if (e.clientY > minifiedMenu.offsetHeight) {
+        openerClose()
+    }
+
+
+})
+
+searchButtons[0].addEventListener('click', () => {
+    if (!(searchFields[0].classList.contains('searchFieldActive'))) {
+        searchFields[0].classList.add('searchFieldActive')
+    }
+    else {
+        searchInputClose(0)
+    }
+})
+
+searchButtons[1].addEventListener('click', () => {
+    if (!(searchFields[1].classList.contains('searchFieldActive'))) {
+        searchFields[1].classList.add('searchFieldActive')
+        searchButtons[1].innerHTML = "  <i class='fas fa-search'></i> Close Search"
+    }
+    else {
+        searchInputClose(1)
+        searchButtons[1].innerHTML = "  <i class='fas fa-search'></i> Search for items"
 
     }
 })
