5af89c79acac86fd89e5b749ba3961342d92dff2
Bartek Wisniewski <wis.keybar87@gmail.com>
2019-01-24 16:20:16 +0100

    added some js for navigation in mobile and desktop

--- scripts.js
+++ scripts.js
@@ -1,5 +1,6 @@
 const menuOpener = document.querySelector('.top_minified__opener')
-
+const expandedMenu = document.querySelector('.top_expanded');
+const mainNav = document.querySelector('.top_nav')
 menuOpener.addEventListener('click', (e) => {
     const minifiedMenu = document.querySelector('.top_minified');
     if (!(minifiedMenu.classList.contains('top_maximized'))) {
@@ -14,3 +15,19 @@ menuOpener.addEventListener('click', (e) => {
     }
 }
 )
+
+window.addEventListener('scroll', () => {
+    console.log(expandedMenu.offsetHeight, expandedMenu.offsetWidth);
+
+
+    if (pageYOffset >= 150) {
+        expandedMenu.classList.add('top_fixed')
+        mainNav.style.marginTop = `${expandedMenu.offsetHeight}px`
+
+    }
+    else {
+        expandedMenu.classList.remove('top_fixed')
+        mainNav.style.marginTop = 0
+
+    }
+})
