228e93bc7174630c6e6de416ef862011e963611e
Bartek Wisniewski <wis.keybar87@gmail.com>
2019-01-27 17:41:03 +0100

    decided to go for a broader variety of pictures, added JS for currency changer and trending section animation

--- scripts.js
+++ scripts.js
@@ -6,6 +6,50 @@ const menuElements = Array.from(document.querySelectorAll('.top_minified__item')
 const searchButtons = Array.from(document.querySelectorAll('.searchItems'))
 const searchFields = Array.from(document.querySelectorAll('.searchField'))
 const newItemsParagraphTwo = document.querySelector('.newItems_paragraphTwo');
+const trendingItemsContainer = document.querySelector('.trendingItems_container');
+const trendingItemsGalleryBtn = document.querySelector('.trendingItems_showGallery');
+const convertMoneyBtnsTop = Array.from(document.querySelectorAll('.convertMoneyBtnTop'))
+const convertMoneyBtnMin = Array.from(document.querySelectorAll('.convertMoneyBtnMin'))
+const convertMoneyBtnTxt = document.querySelector('.convertMoneyBtnTxt');
+
+convertMoneyBtnsTop.forEach(convertBtn => {
+    convertBtn.addEventListener('click', (e) => {
+        if (convertMoneyBtnsTop.filter(btn => btn.classList.contains('visible')).length == convertMoneyBtnsTop.length) {
+            convertMoneyBtnsTop.forEach(btn => btn.classList.remove('visible'));
+            convertMoneyBtnsTop.forEach(btn => btn.classList.add('invisible'));
+            convertMoneyBtnsTop[e.target.dataset.btnkey].classList.add('visible');
+
+            convertMoneyBtnMin.forEach(btnMin => btnMin.style.color = 'white');
+            convertMoneyBtnMin[e.target.dataset.btnkey].style.color = 'orangered';
+        }
+        else if (!(convertMoneyBtnsTop.filter(btn => btn.classList.contains('visible')).length == convertMoneyBtnsTop.length)) {
+            convertMoneyBtnsTop.forEach(btn => btn.classList.remove('invisible'));
+            convertMoneyBtnsTop.forEach(btn => btn.classList.add('visible'));
+        }
+
+    })
+})
+
+
+convertMoneyBtnMin.forEach(convertBtn => {
+    convertBtn.addEventListener('click', (e) => {
+        convertMoneyBtnMin.forEach(btnMin => btnMin.style.color = 'white');
+        e.target.style.color = "orangered";
+        convertMoneyBtnsTop.forEach(btn => btn.classList.remove('visible'));
+        convertMoneyBtnsTop.forEach(btn => btn.classList.add('invisible'));
+        convertMoneyBtnsTop[e.target.dataset.btnkey].classList.add('visible');
+    })
+
+
+})
+
+
+convertMoneyBtnTxt.addEventListener('click', () => {
+    convertMoneyBtnMin.forEach(btn => btn.style.boxShadow = "0px 0px 2px inset orangered")
+    const shadowRemove = () => { convertMoneyBtnMin.forEach(btn => btn.style.boxShadow = "none") }
+    setTimeout(shadowRemove, 1000)
+})
+
 
 
 
@@ -77,6 +121,22 @@ searchButtons[1].addEventListener('click', (e) => {
     else {
         searchInputClose(searchButtonIndex)
         searchButtons[searchButtonIndex].innerHTML = "  <i class='fas fa-search'></i> Search for items"
+
+    }
+})
+
+
+
+trendingItemsGalleryBtn.addEventListener('click', () => {
+    if (!(trendingItemsContainer.classList.contains('trendingItems_container_active'))) {
+        trendingItemsContainer.classList.add('trendingItems_container_active');
+        window.scrollTo(0, (trendingItemsContainer.offsetTop * 0.95))
+        trendingItemsGalleryBtn.textContent = "close"
+    }
+    else if (trendingItemsContainer.classList.contains('trendingItems_container_active')) {
+        console.log('nie ma')
+        trendingItemsContainer.classList.remove('trendingItems_container_active')
+        trendingItemsGalleryBtn.textContent = "see what's trending"
 
     }
 })
