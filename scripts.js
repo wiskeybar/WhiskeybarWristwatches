429ad14b376498a2b861f829629ec37e7d2a8b79
Bartek Wisniewski <wis.keybar87@gmail.com>
2019-02-01 21:36:24 +0100

    added most of wanted JS nad CSS, testing and code-cleanup left

--- scripts.js
+++ scripts.js
@@ -23,51 +23,72 @@ const wrapper = document.querySelector('.wrapper');
 const galleryItems = Array.from(document.querySelectorAll('.watchGallery_galleryItem'));
 const seeMoreBtn = Array.from(document.querySelectorAll('.newItems_examplePicContainer__seeMoreBtn'));
 const watchGallerySub = Array.from(document.querySelectorAll('.watchGallery_gallerySub'));
-const shoppingBag = Array.from(document.querySelectorAll('.shoppingBag'));
+const shoppingBag = document.querySelector('.shoppingBag');
 const basket = document.querySelector('.basket');
-const basketMinified = document.querySelector('.basket_minified');
 const addNewItemBtns = Array.from(document.querySelectorAll('.newItems_add'));
+const itemsInBasket = []
+
+
+
+class Basket {
+
+    constructor(quantityOfItems, nameOfItem) {
+        this.quantityOfItems = quantityOfItems
+        this.nameOfItem = nameOfItem
+    }
+
+
+    addToBasket(path, price, description, quantityValue) {
+        const itemMaximized = document.createElement('div');
+
+        const img = document.createElement('img');
+        const itemPrice = document.createElement('p')
+
+        const itemDescription = document.createElement('p');
+        const quantity = document.createElement('input');
+
+
+        itemMaximized.dataName = description
+
+        itemMaximized.className = "basketItemMaximized";
+        img.className = "basketImage";
+        itemDescription.className = "basketDescription";
+        quantity.className = "basketQuantity";
+        quantity.type = "number"
+        img.src = path;
+        itemDescription.textContent = description;
+        itemPrice.textContent = parseFloat(price);
+        itemPrice.className = 'basketPrice';
+        itemPrice.dataPrice = parseFloat(price)
+        quantity.value = quantityValue;
+        basket.prepend(itemMaximized)
+
+        itemMaximized.prepend(itemPrice)
+        itemMaximized.prepend(quantity)
+        itemMaximized.prepend(img)
+        itemMaximized.prepend(itemDescription)
+
+        quantity.addEventListener('change', (e) => {
+            if (quantity.value == 0) {
+                e.target.parentNode.remove()
+            }
+            itemPrice.textContent = (itemPrice.dataPrice * quantity.value).toFixed(2);
+
+        })
 
 
 
 
 
-
-
-
-const modalCreator = (e) => {
-    let src = e.target.src
-    let newSrc = src.replace(/720/gi, 1920)
-
-    modal.className = "modalPopup";
-    modal.src = newSrc
-    modal.style.maxWidth = "80%";
-    modal.style.maxHeight = "80%";
-    modal.style.margin = "0 auto"
-    wrapper.classList.add('blurred')
-
-
-    modalContainer.appendChild(modal);
-    modalClose.className = "modalClose"
-    modalClose.innerHTML = "<p> CLOSE GALLERY </p>";
-    modalContainer.appendChild(modalClose);
+    }
 
 }
-const menuOpenerClose = () => {
-    minifiedMenu.classList.remove('top_maximized');
-    menuOpener.innerHTML = "<i class='fas fa-bars'>";
-    menuOpener.style.color = "black";
-    menuOpener.style.left = "1%";
 
-
-}
-const searchInputClose = (i) => {
-    searchFields[i].classList.remove('searchFieldActive')
-}
-
+// allow currency change for desktop view
 convertMoneyBtnsTop.forEach(convertBtn => {
     convertBtn.addEventListener('click', (e) => {
         if (convertMoneyBtnsTop.filter(btn => btn.classList.contains('visible')).length == convertMoneyBtnsTop.length) {
+
             convertMoneyBtnsTop.forEach(btn => btn.classList.remove('visible'));
             convertMoneyBtnsTop.forEach(btn => btn.classList.add('invisible'));
             convertMoneyBtnsTop[e.target.dataset.btnkey].classList.add('visible');
@@ -82,6 +103,7 @@ convertMoneyBtnsTop.forEach(convertBtn => {
 
     })
 })
+// allow currency change for mobile view
 convertMoneyBtnMin.forEach(convertBtn => {
     convertBtn.addEventListener('click', (e) => {
         convertMoneyBtnMin.forEach(btnMin => btnMin.style.color = 'white');
@@ -93,27 +115,14 @@ convertMoneyBtnMin.forEach(convertBtn => {
 
 
 })
-
-
+// turns suggestion for currency change on/off
 convertMoneyBtnTxt.addEventListener('click', () => {
     convertMoneyBtnMin.forEach(btn => btn.style.boxShadow = "0px 0px 2px inset orangered")
     const shadowRemove = () => { convertMoneyBtnMin.forEach(btn => btn.style.boxShadow = "none") }
     setTimeout(shadowRemove, 1000)
 })
 
-menuOpener.addEventListener('click', () => {
-    if (!(minifiedMenu.classList.contains('top_maximized'))) {
-        minifiedMenu.classList.add('top_maximized');
-        menuOpener.innerHTML = "<i class='fas fa-times'>";
-        menuOpener.style.color = "white";
-        menuOpener.style.left = "95%";
-    }
-    else {
-        menuOpenerClose()
-    }
-}
-)
-
+// adds an animation for one paragraph and controles top menu in dekstop view
 window.addEventListener('scroll', () => {
 
     if (pageYOffset >= 150) {
@@ -129,15 +138,39 @@ window.addEventListener('scroll', () => {
         mainNav.style.marginTop = 0;
     }
 })
-
-window.addEventListener('click', (e) => {
-    if (e.clientY > minifiedMenu.offsetHeight) {
-        menuOpenerClose()
+// changes the bars and X for minified view, functions stored in variables
+const menuClose = () => {
+    minifiedMenu.classList.remove('top_maximized');
+    menuOpener.innerHTML = "<i class='fas fa-bars'>";
+    menuOpener.style.color = "black";
+    menuOpener.style.left = "1%";
+}
+const menuOpen = () => {
+    minifiedMenu.classList.add('top_maximized');
+    menuOpener.innerHTML = "<i class='fas fa-times'>";
+    menuOpener.style.color = "white";
+    menuOpener.style.left = "92%";
+}
+menuOpener.addEventListener('click', () => {
+    if (!(minifiedMenu.classList.contains('top_maximized'))) {
+        menuOpen()
     }
-
-
+    else {
+        menuClose()
+    }
 })
 
+// in minified view closes the sidebar menu when event is outside of the menubox, see above for function
+window.addEventListener('click', (e) => {
+    if (e.clientY > minifiedMenu.offsetHeight) {
+        menuClose()
+    }
+})
+
+// operates the searchboxes
+const searchInputClose = (i) => {
+    searchFields[i].classList.remove('searchFieldActive')
+}
 searchButtons[0].addEventListener('click', (e) => {
     const searchButtonIndex = searchButtons.indexOf(e.target)
 
@@ -162,7 +195,7 @@ searchButtons[1].addEventListener('click', (e) => {
 
     }
 })
-
+// in mobile view allows to hide the trending items container
 trendingItemsGalleryBtn.addEventListener('click', () => {
     if (!(trendingItemsContainer.classList.contains('trendingItems_container_active'))) {
         trendingItemsContainer.classList.add('trendingItems_container_active');
@@ -175,8 +208,7 @@ trendingItemsGalleryBtn.addEventListener('click', () => {
 
     }
 })
-
-
+// operates hints for input in contact form
 submitFormBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
@@ -201,17 +233,21 @@ submitFormBtn.addEventListener('click', (e) => {
     }
 
     else {
-        alert("No backend on the site, sorry. It ain't much, but it's honest work :)")
+        alert(`No backend on the site, sorry. 
+                        It ain't much, but it's honest work :)`)
     }
 
 })
 
-galleryItems.forEach(item => item.addEventListener('click', e => { modalCreator(e) }))
-
+// these are for opening and closing popup modal gallery
 modalContainer.addEventListener('click', (e) => {
+
+    // this gets the number of current, next and previous picture from the src attribute
     let modalnumber = modal.src.lastIndexOf(modal.src.charAt(modal.src.length - 6));
     let newSrcRight = Number(modal.src.charAt(modal.src.length - 6)) + 1;
     let newSrcLeft = Number(modal.src.charAt(modal.src.length - 6)) - 1;
+
+    // these functions allow for navigating inside modals
     if (e.clientX > (((e.target.getBoundingClientRect().width / 2) + e.target.getBoundingClientRect().x))) {
         if (Number(modal.src.charAt(modal.src.length - 6))
             !== galleryItems.length) {
@@ -240,92 +276,20 @@ modalClose.addEventListener('click', () => {
     wrapper.classList.remove('blurred')
 })
 
-seeMoreBtn.forEach(btn => btn.addEventListener('click', () => { window.scrollTo(0, (galleryItems[0].offsetTop * 0.95)) }))
-watchGallerySub.forEach(btn => btn.addEventListener('click', () => { window.scrollTo(0, (trendingItemsContainer.offsetTop * 0.95)) }))
-
-
-class Basket {
-
-    addToBasket(path, price, description) {
-        const itemMaximized = document.createElement('div');
-        const itemMinified = document.createElement('div');
-
-        const img = document.createElement('img');
-        const itemPrice = document.createElement('p')
-
-        const itemDescription = document.createElement('p');
-        const quantity = document.createElement('input');
-
-        const imgMin = document.createElement('img');
-        const itemDescriptionMin = document.createElement('p');
-        const itemPriceMin = document.createElement('p')
-        const quantityMin = document.createElement('input');
-
-
-        itemMaximized.dataName = description
-
-        itemMaximized.className = "basketItemMaximized";
-        itemMinified.className = "basketItemMinified"
-        img.className = "basketImage";
-        itemDescription.className = "basketDescription";
-        quantity.className = "basketQuantity";
-        quantity.type = "number"
-        img.src = path;
-        itemDescription.textContent = description;
-        itemPrice.textContent = parseFloat(price);
-        itemPrice.className = 'basketPrice';
-        itemPrice.dataPrice = parseFloat(price)
-        quantity.value = 1;
-        quantity.addEventListener('change', () => {
-            itemPrice.textContent = (itemPrice.dataPrice * quantity.value).toFixed(2)
-        })
-
-
-        imgMin.className = "basketImage";
-        itemDescriptionMin.className = "basketDescription";
-        quantityMin.className = "basketQuantity";
-        imgMin.src = path;
-        itemDescriptionMin.textContent = description;
-        itemPriceMin.textContent = parseFloat(price);
-        itemPriceMin.className = 'basketPrice';
-        itemPriceMin.dataPrice = parseFloat(price)
-
-        quantityMin.value = 1;
-
-
-
-        basket.prepend(itemMaximized)
-        basketMinified.prepend(itemMinified)
-
-        itemMaximized.prepend(itemPrice)
-        itemMaximized.prepend(quantity)
-        itemMaximized.prepend(img)
-        itemMaximized.prepend(itemDescription)
-
-
-        itemMinified.prepend(itemPriceMin)
-        itemMinified.prepend(quantityMin)
-        itemMinified.prepend(imgMin)
-        itemMinified.prepend(itemDescriptionMin)
-
-
-
-    }
-
-}
-
-const itemsInBasket = []
-
+// adding items inside basket. The basket is visable only in fullscreen/desktop mode.
 addNewItemBtns.forEach(btn => btn.addEventListener('click', e => {
+
+    // variables for easier navigation inside the function
     const newItem = new Basket;
     const parent = e.target.parentNode;
     const sources = Array.from(parent.childNodes);
+    const quantity = 1;
     const imgSrc = sources.filter(source => source.src);
     const price = sources.filter(source => source.title)
     const currentname = parent.dataset['name']
     const basketItems = Array.from(basket.querySelectorAll('.basketItemMaximized'));
 
-
+    // prevents adding new lines inside basket when item is already in it
     if (itemsInBasket.includes(currentname)) {
         const inBasket = basketItems.filter(item => item.dataName === currentname);
         let inBasketPrice = inBasket[0].childNodes[3].dataPrice;
@@ -333,8 +297,10 @@ addNewItemBtns.forEach(btn => btn.addEventListener('click', e => {
         let basketInput = inBasket[0].childNodes[2]
         basketInput.value = inBasketQuantity + 1
 
+        // for instances of first 'add' and all the next
         if (inBasketPrice === Number(inBasket[0].childNodes[3].textContent)) {
             inBasket[0].childNodes[3].textContent = inBasketPrice * 2
+
         }
         else {
             inBasketQuantity += 1
@@ -343,17 +309,47 @@ addNewItemBtns.forEach(btn => btn.addEventListener('click', e => {
 
 
     }
+    // adds a new item based on variables and Basket class
     else {
-        newItem.addToBasket(imgSrc[0].src, price[0].textContent, currentname)
+        newItem.addToBasket(imgSrc[0].src, price[0].textContent, currentname, quantity)
         itemsInBasket.push(currentname)
-
+        this.nameOfItem = currentname
     }
+    const bagAnimation = () => shoppingBag.classList.remove('shoppingBag_animated');
+    shoppingBag.classList.add('shoppingBag_animated');
+    setTimeout(bagAnimation, 200);
 
 }))
 
+// opens/closes the basket
 
-shoppingBag.forEach(button => button.addEventListener('click', () => {
-    basket.classList.contains('basket_open') ? basket.classList.remove('basket_open') : basket.classList.add('basket_open');
-    basketMinified.classList.contains('basket_open') ? basketMinified.classList.remove('basket_open') : basketMinified.classList.add('basket_open');
-}))
+shoppingBag.addEventListener('click', () => {
+    basket.classList.contains('basket_open') ? (basket.classList.remove('basket_open'), shoppingBag.style.color = 'black') : (basket.classList.add('basket_open'), shoppingBag.style.color = 'orangered');
+})
+
+// send the user to a specific spot on the page
+seeMoreBtn.forEach(btn => btn.addEventListener('click', () => { window.scrollTo(0, (galleryItems[0].offsetTop * 0.95)) }));
+
+watchGallerySub.forEach(btn => btn.addEventListener('click', () => { window.scrollTo(0, (trendingItemsContainer.offsetTop * 0.95)) }))
+
+// creates a modal popup for gallery
+const modalCreator = (e) => {
+    let src = e.target.src
+    let newSrc = src.replace(/720/gi, 1920)
+
+    modal.className = "modalPopup";
+    modal.src = newSrc
+    modal.style.maxWidth = "80%";
+    modal.style.maxHeight = "80%";
+    modal.style.margin = "0 auto"
+    wrapper.classList.add('blurred')
+
+
+    modalContainer.appendChild(modal);
+    modalClose.className = "modalClose"
+    modalClose.innerHTML = "<p> CLOSE GALLERY </p>";
+    modalContainer.appendChild(modalClose);
+
+}
+galleryItems.forEach(item => item.addEventListener('click', e => { modalCreator(e) }))
 
